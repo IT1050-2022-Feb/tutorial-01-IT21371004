2c992624952bdb339dd59d77b545e3ead7d46db7
Niflan M.N.A <99314244+IT21371004@users.noreply.github.com>
2022-02-09 06:58:51 +0000

    have finished tute 1

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,18 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
+  float subject1, subject2 ;
+  float average, total;
+   
+  printf("Enter marks of two subject :\n ");
+  scanf("%f%f", &subject1, &subject2);
+
+  total = subject1 + subject2 ;
+  average = total/2;
+
+  printf ("Average marks = %.2f\n", average);
   
   return 0;
 }
